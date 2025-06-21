287696a2f49097ab0cc1370c0ab3fa291eda17e0
AJ-Holzer <axel123.holzer@gmail.com>
2025-06-21 12:52:33 +0200

    Remove disabled actions from trunk.yaml and clean up compile_apk.bat script

--- compile_apk.bat
+++ compile_apk.bat
@@ -8,8 +8,9 @@ flet build apk ^
     --product "ZEPHRA" ^
     --org "sites.ajservers" ^
     --source-packages certifi pyaes argon2 ^
+    --clear-cache ^
+    --exclude ".trunk" ^
     -v
-    @REM --clear-cache ^
     @REM --cleanup-packages ^
     @REM --compile-packages ^
     @REM --android-permissions android.permission.READ_EXTERNAL_STORAGE=True,android.permission.WRITE_EXTERNAL_STORAGE=True,android.permission.INTERNET=True ^
