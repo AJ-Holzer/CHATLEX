9e2df40d90e8a68478466e90536e95d22931b62d
AJ-Holzer <axel123.holzer@gmail.com>
2025-07-04 21:20:20 +0200

    Refactor: Remove commented-out AES-256-CBC implementation to clean up encryption module

--- env/classes/encryption.py
+++ env/classes/encryption.py
@@ -8,10 +8,6 @@ from env.config import config
 from env.func.generations import generate_iv, generate_salt
 from env.typing.hashing import HKDFInfoKey
 
-# from cryptography.hazmat.primitives.padding import PaddingContext
-# from cryptography.hazmat.primitives import padding
-# from cryptography.hazmat.primitives.ciphers import CipherContext
-
 
 class AES_256_GCM:
     def __init__(self, derived_key: bytes) -> None:
@@ -64,56 +60,3 @@ class AES_256_GCM:
         decryptor: AEADDecryptionContext = cipher.decryptor()
         plaintext_bytes: bytes = decryptor.update(ciphertext) + decryptor.finalize()
         return plaintext_bytes.decode(config.ENCODING)
-
-
-# class AES_256_CBC:
-#     def __init__(self, derived_key: bytes) -> None:
-#         self._derived_key: bytes = derived_key
-#         self._hkdf_hasher: HKDFHasher = HKDFHasher(derived_key=self._derived_key)
-
-#     def encrypt(self, plaintext: str, encryption_key_info: HKDFInfoKey) -> bytes:
-#         iv: bytes = generate_iv(length=config.AES_256_CBC_IV_LENGTH)
-#         salt: bytes = generate_salt(length=config.SALT_LENGTH)
-#         encryption_key: bytes = self._hkdf_hasher.derive_key(
-#             info=encryption_key_info,
-#             salt=salt,
-#         )
-
-#         # Create padder
-#         padder: PaddingContext = padding.PKCS7(block_size=128).padder()
-#         padded_data = (
-#             padder.update(plaintext.encode(config.ENCODING)) + padder.finalize()
-#         )
-
-#         # Create cipher text
-#         cipher: Cipher[modes.CBC] = Cipher(
-#             algorithm=algorithms.AES256(encryption_key),
-#             mode=modes.CBC(iv),
-#             backend=default_backend(),
-#         )
-#         encryptor: CipherContext = cipher.encryptor()
-#         ciphertext = encryptor.update(padded_data) + encryptor.finalize()
-
-#         return salt + iv + ciphertext
-
-#     def decrypt(self, encrypted_data: bytes, encryption_key_info: HKDFInfoKey) -> str:
-#         salt: bytes = encrypted_data[: config.SALT_LENGTH]
-#         iv: bytes = encrypted_data[config.SALT_LENGTH : config.SALT_LENGTH + 16]
-#         ciphertext: bytes = encrypted_data[config.SALT_LENGTH + 16 :]
-
-#         decryption_key: bytes = self._hkdf_hasher.derive_key(
-#             info=encryption_key_info,
-#             salt=salt,
-#         )
-
-#         cipher: Cipher[modes.CBC] = Cipher(
-#             algorithm=algorithms.AES(decryption_key),
-#             mode=modes.CBC(iv),
-#             backend=default_backend(),
-#         )
-#         decryptor: CipherContext = cipher.decryptor()
-#         padded_data: bytes = decryptor.update(ciphertext) + decryptor.finalize()
-
-#         unpadder: PaddingContext = padding.PKCS7(128).unpadder()
-#         plaintext: bytes = unpadder.update(padded_data) + unpadder.finalize()
-#         return plaintext.decode(config.ENCODING)
